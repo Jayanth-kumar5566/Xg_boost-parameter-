3ba2b11e2292b725383e99142d04e2d554d03aad
Jayanth-kumar5566 <jayanth.kumar@students.iiserpune.ac.in>
2017-04-06 11:30:01 +0530

    added plotting function

--- classifier.py
+++ classifier.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy.sparse
 import pickle
 import xgboost as xgb
+import pylab
+import time
 #--------Read a csv file and obtainig a DMatrix------------------
 import pandas
 df=pandas.read_csv("pima-data-orig.csv")
@@ -19,12 +21,15 @@ dtrain = xgb.DMatrix(train_data,label=train_label)
 dtest = xgb.DMatrix(test_data,label=test_label)
 
 # specify parameters via map, definition are same as c++ version
-param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
 
+#param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic','eval_metric':'logloss','eval_metric':'error' }
+
+param = [('max_depth', 2), ('objective', 'binary:logistic'), ('eval_metric', 'logloss'), ('eval_metric', 'error'),('eval_metric','auc'),('missing',0)]
+ 
 # specify validations set to watch performance
 watchlist  = [(dtest,'eval'), (dtrain,'train')]
 results=dict()
-num_round = 100
+num_round = 50
 bst = xgb.train(param, dtrain, num_round, watchlist,evals_result=results)
 
 # this is prediction
@@ -33,4 +38,35 @@ labels = dtest.get_label()
 print ('error=%f' % ( sum(1 for i in range(len(preds)) if int(preds[i]>0.5)!=labels[i]) /float(len(preds))))
 
 #-------------To set up evaluvation of models----------------------------------
+
+#Print model report:
+print "\nModel Report"
+print "\nTrain-Accuracy"
+ter=results['train']['error'][-1]
+tauc=results['train']['auc'][-1]
+print (1-float(ter))*100
+print "AUC Score(Train)",tauc
+print "\nTest-Accuracy"
+er=results['eval']['error'][-1]
+auc=results['eval']['auc'][-1]
+print (1-float(er))*100
+print "AUC Score(Test)",auc
+
+#-------------------Graphical Plotting----------------------------
+'''pylab.ion()
+x=0
+for i in results['train']['error']:
+    pylab.plot(x,float(i),marker='o', linestyle='--')
+    x+=1
+    pylab.draw()
+    time.sleep(0.2)'''
+
+pylab.plot(range(len(results['train']['error'])),results['train']['error'])
+pylab.show()
+
+pylab.plot(range(len(results['train']['auc'])),results['train']['auc'])
+pylab.show()
+
+#---------------Feature Importance--------------------------
+
 
